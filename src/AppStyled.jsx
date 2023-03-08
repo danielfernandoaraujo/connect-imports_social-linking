d776e594da8de19f2895ff17071535891cb72668
danielfernandoaraujo <danielferna11@gmail.com>
2023-03-07 23:11:24 -0300

    banner and icone positioned

--- src/AppStyled.jsx
+++ src/AppStyled.jsx
@@ -1,40 +1,80 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const animateBorder = keyframes`
+   from {
+    border-color: transparent;
+  }
+  to {
+    border-color: #FF0059;
+  }
+`;
 
 export const Main = styled.div`
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   flex-direction: column;
   .img-banner-top {
+    height: 40vh;
+    width: 100vw;
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
     img {
+      height: 35vh;
+      width: 40vw;
       border-radius: 0 0 45px 45px;
-    }
-    @media screen and (max-width: 768px) {
-      width: 100vw;
+      @media screen and (max-width: 768px) {
+        height: 35vh;
+        width: 75vw;
+      }
+      @media screen and (max-width: 480px) {
+        height: 35vh;
+        width: 100vw;
+      }
     }
   }
   .img-icon {
     position: absolute;
-
+    top: 20%;
+    left: 50%;
+    transform: translateX(-50%);
     img {
+      height: 20vh;
       border-radius: 100%;
+      border: 2px solid transparent;
+      border-radius: 50%;
+      animation: ${animateBorder} 1.4s forwards;
     }
     @media screen and (max-width: 768px) {
-      width: 100vw;
-      top: 10%;
+    }
+    @media screen and (max-width: 480px) {
+      top: 31%;
+      left: 50%;
+
+      transform: translate(-50%, -50%);
     }
   }
 
-  /* Define as propriedades de responsividade */
   @media screen and (max-width: 768px) {
-    max-width: 90%;
-    padding: 10px;
   }
 
   @media screen and (max-width: 480px) {
-    max-width: 100%;
-    padding: 5px;
+  }
+  @keyframes pulse {
+    0% {
+      transform: scale(1);
+      opacity: 1;
+    }
+    50% {
+      transform: scale(1.2);
+      opacity: 0.7;
+    }
+    100% {
+      transform: scale(1);
+      opacity: 1;
+    }
   }
 `;
